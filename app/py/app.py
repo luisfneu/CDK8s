f3c7591e8932b40653af81111684023a020b5dee
Luis Fernando Neu <luisneu@gmail.com>
2026-01-11 17:26:48 -0300

    python

---
+++ app/py/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return "ok", 200
+
+@app.route("/sum")
+def soma():
+    try:
+        a = float(request.args.get("a", 0))
+        b = float(request.args.get("b", 0))
+        return jsonify({
+            "a": a,
+            "b": b,
+            "resultado": a + b
+        })
+    except:
+        return jsonify({"erro": "error error error"}), 400
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
